18345d176da57df58ce8ca993a3232276aa50d2f
MinChangs <bijou0147@gmail.com>
2019-07-09 09:30:32 +0900

    spring batch  발효유 일실적 생성 배치   - reader, writer : Mybatis batch 사용   - processor : CycleVo -> List<DailyVo>   - 요일 체크 로직 추가

--- src/test/java/kr/or/ddit/batch/YogultBatchTest.java
+++ src/test/java/kr/or/ddit/batch/YogultBatchTest.java
@@ -9,34 +9,45 @@ import org.junit.runner.RunWith;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:kr/or/ddit/config/spring/application-batch-dev.xml"})
-public class RangerBatchTest {
+@ContextConfiguration(locations = {
+						"classpath:kr/or/ddit/config/spring/application-datasource-dev.xml",
+						"classpath:kr/or/ddit/config/spring/application-transaction.xml",
+						"classpath:kr/or/ddit/config/spring/application-batch-dev.xml"})
+public class YogultBatchTest {
 	@Resource(name = "jobLauncher")
 	private JobLauncher jobLauncher;
 	
-	@Resource(name = "rangerJob")
-	private Job rangerJob;
+	@Resource(name = "yogultJob")
+	private Job yogultJob;
+
 	
 	/**
-	* Method : rangerBatchTest
+	* Method : yogultBatchTest
 	* 작성자 : PC24
 	* 변경이력 :
 	* @throws Exception
-	* Method 설명 : 레인저스 이름 수정 배치 테스트
+	* Method 설명 : 발효유 일실적 생성배치
 	*/
 	@Test
-	public void rangerBatchTest() throws Exception {
+	public void yogultBatchTest() throws Exception {
 		/***Given***/
+		String st = "201907";
 		
 
 		/***When***/
-		JobExecution jobExecution=jobLauncher.run(rangerJob, new JobParameters());
+		JobExecution jobExecution=jobLauncher.run(yogultJob, 
+								new JobParametersBuilder().addParameter("ym", new JobParameter(st))
+								.addLong("stard_dt", System.currentTimeMillis())
+								.toJobParameters()
+								);
 		/***Then***/
 		assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
 	}
